7974977ebb01a0b6102b32b8561f8b448971859c
baojieren <2013142083@qq.com>
2020-06-11 13:47:26 +0800

    init

---
+++ src/main/java/ink/baojie/docker/DockerApplication.java
@@ -0,0 +1,21 @@
+package ink.baojie.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+
+    @GetMapping("docker")
+    public String docker(String msg) {
+        System.out.println("接口接收到:" + msg);
+        return "你输入的是" + msg;
+    }
+}
